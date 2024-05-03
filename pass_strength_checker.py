f7f6547f293a4bed48cc213cf96887945d018d11
Pevin_the_hacker <86566509+Bevin007@users.noreply.github.com>
2024-05-03 13:46:28 +0530

    Add files via upload

---
+++ pass_strength_checker.py
@@ -0,0 +1,31 @@
+only_num=False
+only_lett=False
+mix_num_and_lett=False
+with_spec_chr=False
+"""def num_or_char(passwd):
+	for ch in passwd:
+		if ch.isdigit():
+			only_num=True"""
+			
+#function of finding low or up or both case in given password
+
+def low_or_up(passwd):
+			global only_lett_in_low
+			global only_lett_in_up
+			global contain_low_and_up
+			only_lett_in_low=False
+			only_lett_in_up=False
+			contain_low_and_up=False
+			for ch in passwd:
+				if ch.islower():
+					only_lett_in_low=True
+				if ch.isupper():
+					only_lett_in_up=True
+				if only_lett_in_low and only_lett_in_up:
+					contain_low_and_up=True
+passwd=input("Enter your password: ")
+len=len(passwd)
+if len(passwd==0):
+	print("Please enter your password :")
+else:
+	low_or_up(passwd)
