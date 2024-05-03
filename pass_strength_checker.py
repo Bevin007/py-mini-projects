1d9aca77bd11911af72adb89098d0036a6e7ac38
Pevin_the_hacker <86566509+Bevin007@users.noreply.github.com>
2024-05-03 13:48:54 +0530

    Update pass_strength_checker.py

--- pass_strength_checker.py
+++ pass_strength_checker.py
@@ -10,22 +10,22 @@ with_spec_chr=False
 #function of finding low or up or both case in given password
 
 def low_or_up(passwd):
-			global only_lett_in_low
-			global only_lett_in_up
-			global contain_low_and_up
-			only_lett_in_low=False
-			only_lett_in_up=False
-			contain_low_and_up=False
-			for ch in passwd:
-				if ch.islower():
-					only_lett_in_low=True
-				if ch.isupper():
-					only_lett_in_up=True
-				if only_lett_in_low and only_lett_in_up:
-					contain_low_and_up=True
+	global only_lett_in_low
+	global only_lett_in_up
+	global contain_low_and_up
+	only_lett_in_low=False
+	only_lett_in_up=False
+	contain_low_and_up=False
+	for ch in passwd:
+		if ch.islower():
+			only_lett_in_low=True
+		if ch.isupper():
+			only_lett_in_up=True
+		if only_lett_in_low and only_lett_in_up:
+			contain_low_and_up=True
 passwd=input("Enter your password: ")
-len=len(passwd)
-if len(passwd==0):
+length=len(passwd)
+if (length==0):
 	print("Please enter your password :")
 else:
 	low_or_up(passwd)
